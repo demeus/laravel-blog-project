9fc3d7324e4059340d60a281f932a4124ade44d4
dmitriy.t <dmitriy.t@numeto.com>
2023-11-03 15:31:24 +0100

    Add new models, migrations, mail class and language lines

---
+++ app/Filament/Resources/UserResource/Widgets/UserChart.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Filament\Resources\UserResource\Widgets;
+
+use App\Models\User;
+use Filament\Widgets\ChartWidget;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+
+class UserChart extends ChartWidget
+{
+    protected static string|null $heading = 'Chart';
+
+    protected function getData(): array
+    {
+        // Totals per month
+        $data = Trend::model(User::class)
+            ->between(
+                start: now()->startOfMonth(),
+                end: now()->endOfMonth(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'User Registered',
+                    'data' => $data->map(fn(TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $data->map(fn(TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
