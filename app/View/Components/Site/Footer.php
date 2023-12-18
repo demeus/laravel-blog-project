7433dd78885d4d71922f0341ff2f3d931de5dac8
dmitriy.t <dmitriy.t@numeto.com>
2023-12-18 11:24:34 +0100

    Refactor Settings

--- app/View/Components/Site/Footer.php
+++ app/View/Components/Site/Footer.php
@@ -2,10 +2,11 @@
 
 namespace App\View\Components\Site;
 
-use Closure;
-use Illuminate\View\Component;
 use App\Settings\FooterSettings;
+use Closure;
 use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+use Override;
 
 class Footer extends Component
 {
@@ -20,27 +21,33 @@ class Footer extends Component
     /**
      * Get the view / contents that represent the component.
      */
-    #[\Override]
-    public function render() : View|Closure|string
+    #[Override]
+    public function render(): View|Closure|string
     {
         return view('components.site.footer', [
-            'links' => $this->getFooterLinksProperty(),
+            'links'          => $this->getFooterLinksProperty(),
+            'social_links'   => $this->getFooterSocialLinksProperty(),
             'show_copyright' => $this->showCopyrightProperty(),
-            'copyright' => $this->getCopyrightProperty(),
+            'copyright'      => $this->getCopyrightProperty(),
         ]);
     }
 
-    private function getFooterLinksProperty() : ?array
+    private function getFooterLinksProperty(): array|null
     {
         return app(FooterSettings::class)->links;
     }
 
-    private function getCopyrightProperty() : ?string
+    private function getFooterSocialLinksProperty(): array|null
+    {
+        return app(FooterSettings::class)->social_links;
+    }
+
+    private function getCopyrightProperty(): string|null
     {
         return app(FooterSettings::class)->copyright;
     }
 
-    private function showCopyrightProperty() : ?string
+    private function showCopyrightProperty(): string|null
     {
         return app(FooterSettings::class)->show_copyright;
     }
